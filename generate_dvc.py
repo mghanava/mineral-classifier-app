943ae28532f81362462a7e613daf711cc18a50e0
mghanava <mghanava@hotmail.com>
2025-08-30 18:11:56 +0000

    changed pipeline desgin to fine-grained stages instead of cycles to improve speed and efficiency

--- generate_dvc.py
+++ generate_dvc.py
@@ -13,7 +13,7 @@ def load_params():
 
 
 def generate_performance_analysis_stage(cycles):
-    """Generate the performance analysis stage configuration"""
+    """Generate the performance analysis stage configuration."""
     metrics_files = []
     for cycle in cycles:
         metrics_files.extend(
@@ -32,56 +32,150 @@ def generate_performance_analysis_stage(cycles):
     }
 
 
-def generate_dvc_yaml():
+def generate_fine_grained_dvc_yaml():
+    """Generate fine-grained DVC pipeline with individual stages."""
     params = load_params()
     cycles = parse_range(params["cycles"])
+    model = params["default_model"]
 
     stages = {
         "stages": {
             "bootstrap": {
                 "cmd": "python src/stages/generate_base_data.py --cycle 0",
-                "deps": ["src/stages/generate_base_data.py"],
+                "deps": [
+                    "src/stages/generate_base_data.py",
+                    "src/utilities/data_utils.py",
+                ],
                 "params": ["data.base"],
-                "outs": ["results/data/base/cycle_0"],
+                "outs": [
+                    "results/data/base/cycle_0/base_data.pt",
+                    "results/data/base/cycle_0/fold_data.pt",
+                    "results/data/base/cycle_0/test_data.pt",
+                ],
             }
         }
     }
 
     # Generate cycle stages
     for cycle in cycles:
-        stages["stages"][f"cycle_{cycle}"] = {
-            "cmd": f"python src/run_cycle.py --cycle {cycle} --model {params['default_model']}",
+        stages["stages"][f"train_cycle_{cycle}"] = {
+            "cmd": f"python src/stages/train.py --cycle {cycle} --model {model}",
             "deps": [
-                "src/run_cycle.py",
-                "src/stages/",
-                "src/models/",
-                "src/utilities/",
-                f"results/data/base/cycle_{cycle - 1}"
+                "src/stages/train.py",
+                "src/utilities/train_utils.py",
+                "src/utilities/logging_utils.py",
+                f"results/data/base/cycle_{cycle - 1}/fold_data.pt"
                 if cycle > 1
-                else "results/data/base/cycle_0",
+                else "results/data/base/cycle_0/fold_data.pt",
+                "src/models/",
             ],
             "params": [
-                f"models.{params['default_model']}",
-                "data",
+                f"models.{model}",
                 "train",
+            ],
+            "outs": [
+                f"results/trained/cycle_{cycle}",
+            ],
+        }
+        stages["stages"][f"evaluate_cycle_{cycle}"] = {
+            "cmd": f"python src/stages/evaluate.py --cycle {cycle} --model {model}",
+            "deps": [
+                "src/stages/evaluate.py",
+                "src/utilities/eval_utils.py",
+                "src/utilities/logging_utils.py",
+                f"results/data/base/cycle_{cycle - 1}/test_data.pt"
+                if cycle > 1
+                else "results/data/base/cycle_0/test_data.pt",
+                "src/models/",
+            ],
+            "params": [
+                f"models.{model}",
                 "evaluate",
+            ],
+            "outs": [
+                f"results/evaluation/cycle_{cycle}",
+            ],
+        }
+        stages["stages"][f"generate_pred_data_cycle_{cycle}"] = {
+            "cmd": f"python src/stages/generate_pred_data.py --cycle {cycle}",
+            "deps": [
+                "src/stages/generate_pred_data.py",
+                "src/utilities/pred_utils.py",
+                "src/utilities/data_utils.py",
+                f"results/data/base/cycle_{cycle - 1}/base_data.pt"
+                if cycle > 1
+                else "results/data/base/cycle_0/base_data.pt",  # To avoid overlap
+                "src/models/",
+            ],
+            "params": [
+                "data.pred",
+            ],
+            "outs": [
+                f"results/data/prediction/cycle_{cycle}/pred_data.pt",
+            ],
+        }
+        stages["stages"][f"predict_cycle_{cycle}"] = {
+            "cmd": f"python src/stages/predict.py --cycle {cycle} --model {model}",
+            "deps": [
+                "src/stages/predict.py",
+                "src/utilities/pred_utils.py",
+                "src/utilities/logging_utils.py",
+                f"results/data/prediction/cycle_{cycle}/pred_data.pt",
+                f"results/trained/cycle_{cycle}/{model}.pt",
+                f"results/evaluation/cycle_{cycle}/calibrator.pt",
+                "src/models/",
+            ],
+            "params": [
+                f"models.{model}",
+            ],
+            "outs": [
+                f"results/prediction/cycle_{cycle}",
+            ],
+        }
+        stages["stages"][f"analyze_drift_cycle_{cycle}"] = {
+            "cmd": f"python src/stages/analyze_drift.py --cycle {cycle}",
+            "deps": [
+                "src/stages/analyze_drift.py",
+                "src/utilities/drift_detection_utils.py",
+                "src/utilities/logging_utils.py",
+                f"results/data/base/cycle_{cycle - 1}/base_data.pt"
+                if cycle > 1
+                else "results/data/base/cycle_0/base_data.pt",
+                f"results/data/prediction/cycle_{cycle}/pred_data.pt",
+            ],
+            "params": [
                 "analyze_drift",
             ],
             "outs": [
-                f"results/data/base/cycle_{cycle}",
-                f"results/data/prediction/cycle_{cycle}",
-                f"results/trained/cycle_{cycle}",
-                f"results/evaluation/cycle_{cycle}",
                 f"results/drift_analysis/cycle_{cycle}",
             ],
         }
-
+        stages["stages"][f"prepare_next_cycle_data_{cycle}"] = {
+            "cmd": f"python src/stages/prepare_next_cycle_data.py --cycle {cycle}",
+            "deps": [
+                "src/stages/prepare_next_cycle_data.py",
+                "src/utilities/data_utils.py",
+                f"results/data/base/cycle_{cycle - 1}/base_data.pt"
+                if cycle > 1
+                else "results/data/base/cycle_0/base_data.pt",
+                f"results/data/prediction/cycle_{cycle}/pred_data.pt",
+            ],
+            "params": [
+                "data.base",
+            ],
+            "outs": [
+                f"results/data/base/cycle_{cycle}/base_data.pt",
+                f"results/data/base/cycle_{cycle}/fold_data.pt",
+                f"results/data/base/cycle_{cycle}/test_data.pt",
+            ],
+        }
     # Add performance analysis stage
     stages["stages"].update(generate_performance_analysis_stage(cycles))
 
+    # Write to dvc.yaml
     with open("dvc.yaml", "w") as f:
         yaml.dump(stages, f, sort_keys=False, default_flow_style=False)
 
 
 if __name__ == "__main__":
-    generate_dvc_yaml()
+    generate_fine_grained_dvc_yaml()
