identified data leakge when constructing graphs and fixed through subgraph method

src/stages/evaluate.py
@@ -10,7 +10,12 @@ import torch
 
 from src.models import get_model
 from src.utilities.eval_utils import evaluate_with_calibration
-from src.utilities.general_utils import LogTime, ensure_directory_exists, load_params
+from src.utilities.general_utils import (
+    LogTime,
+    ensure_directory_exists,
+    load_data,
+    load_params,
+)
 
 
 def get_cycle_paths(cycle_num):
@@ -46,11 +51,9 @@ def run_evaluation(paths: dict, params: dict, model_name: str):
     model_params["add_self_loops"] = params.get("add_self_loops", True)
     # Ensure output directory exists
     output_path = ensure_directory_exists(paths["output"])
-    # Load test data from previous cycle
-    test_data_path = os.path.join(paths["base"], "test_data.pt")
-    if not os.path.exists(test_data_path):
-        raise FileNotFoundError(f"Test data not found at {test_data_path}")
-    test_data = torch.load(test_data_path, weights_only=False)
+    # Load test and calibration data from previous cycle
+    test_data = load_data(os.path.join(paths["base"], "test_data.pt"))
+    calib_data = load_data(os.path.join(paths["base"], "calib_data.pt"))
 
     # Load trained model
     model_path = os.path.join(paths["model"], "model.pt")
@@ -65,7 +68,8 @@ def run_evaluation(paths: dict, params: dict, model_name: str):
     print(f"Using device {device} ...")
 
     evaluate_with_calibration(
-        data=test_data.to(device),
+        test_data=test_data.to(device),
+        calibration_data=calib_data.to(device),
         model=model.to(device),
         calibration_method=eval_params["calibration_method"],
         initial_temperature=eval_params["initial_temperature"],
@@ -74,19 +78,18 @@ def run_evaluation(paths: dict, params: dict, model_name: str):
         lr=eval_params["lr"],
         n_epochs=eval_params["n_epochs"],
         weight_decay_adam_optimizer=eval_params["weight_decay_adam_optimizer"],
+        reg_lambda=eval_params["reg_lambda"],
+        reg_mu=eval_params["reg_mu"],
+        eps=eval_params["eps"],
         factor_learning_rate_scheduler=eval_params["factor_learning_rate_scheduler"],
         patience_learning_rate_scheduler=eval_params[
             "patience_learning_rate_scheduler"
         ],
         patience_early_stopping=eval_params["patience_early_stopping"],
         min_delta_early_stopping=eval_params["min_delta_early_stopping"],
-        save_path=output_path,
         verbose=eval_params["verbose"],
-        device=device,
-        eps=eval_params["eps"],
-        reg_lambda=eval_params["reg_lambda"],
-        reg_mu=eval_params["reg_mu"],
         seed=eval_params["seed"],
+        save_path=output_path,
     )
     print(f"✓ Evaluation results saved to {output_path}.")
 

src/stages/generate_base_data.py
@@ -14,9 +14,7 @@ import torch
 
 from src.utilities.data_utils import (
     analyze_feature_discrimination,
-    # check_isolated_components,
     construct_graph,
-    # diagnose_data_leakage,
     export_all_graphs_to_html,
     export_graph_to_html,
     generate_mineral_data,
@@ -89,10 +87,7 @@ def prepare_base_data(
         raise ValueError(
             "Expected construct_graph to return a tuple when should_split is True."
         )
-    base_data, fold_data, test_data = all_data
-
-    # diagnose_data_leakage(fold_data, test_data)
-    # check_isolated_components(fold_data, test_data)
+    base_data, fold_data, test_data, calib_data = all_data
 
     # export the interactive 3D plots
     print("\nExporting 3D interactive plots of graphs ...")
@@ -108,9 +103,11 @@ def prepare_base_data(
         labels_map,
         dataset_tag="base_data",
     )
+    # Convert fold_data to list of tuples
     export_all_graphs_to_html(
         fold_data,
         test_data,
+        calib_data,
         coordinates,
         base_params["k_nearest"],
         base_params["connection_radius"],
@@ -132,6 +129,7 @@ def prepare_base_data(
         torch.save(base_data, os.path.join(output_path, "base_data.pt"))
         torch.save(fold_data, os.path.join(output_path, "fold_data.pt"))
         torch.save(test_data, os.path.join(output_path, "test_data.pt"))
+        torch.save(calib_data, os.path.join(output_path, "calib_data.pt"))
         print(f"All files successfully saved to {output_path}.")
     except Exception as e:
         print(f"Error saving files: {e}")

src/stages/predict.py
@@ -10,7 +10,12 @@ import os
 import torch
 
 from src.models import get_model
-from src.utilities.general_utils import LogTime, ensure_directory_exists, load_params
+from src.utilities.general_utils import (
+    LogTime,
+    ensure_directory_exists,
+    load_data,
+    load_params,
+)
 from src.utilities.pred_utils import prediction
 
 
@@ -56,10 +61,7 @@ def run_prediction(paths: dict, params: dict, model_name: str):
     model.load_state_dict(torch.load(model_path, weights_only=True))
 
     # Load prediction data
-    pred_data_path = os.path.join(paths["prediction_data"], "pred_data.pt")
-    if not os.path.exists(pred_data_path):
-        raise FileNotFoundError(f"Prediction data not found at {pred_data_path}")
-    pred_data = torch.load(pred_data_path, weights_only=False)
+    pred_data = load_data(os.path.join(paths["prediction_data"], "pred_data.pt"))
 
     # Load calibrator from evaluation
     calibrator_path = os.path.join(paths["evaluation"], "calibrator.pt")
@@ -71,12 +73,11 @@ def run_prediction(paths: dict, params: dict, model_name: str):
     print(f"Using device {device} ...")
 
     prediction(
-        pred_data=pred_data,
-        model=model,
+        pred_data=pred_data.to(device),
+        model=model.to(device),
         calibrator_path=calibrator_path,
         class_names=base_params["class_names"],
         save_path=output_path,
-        device=device,
     )
     print(f"✓ Prediction results saved to {output_path}.")
 

src/stages/prepare_next_cycle_data.py
@@ -93,7 +93,7 @@ def combine_split_data(
         print("🎯 Applying reservoir sampling to connected graph...")
         pass  # placeholder for reservoir sampling logic
 
-    all_graph_data = construct_graph(
+    all_data = construct_graph(
         coordinates,
         features,
         labels,
@@ -110,11 +110,11 @@ def combine_split_data(
         make_edge_weight_method=params["data"].get("make_edge_weight_method", None),
     )
     # Ensure the returned value is a tuple and unpack accordingly
-    if not isinstance(all_graph_data, tuple):
+    if not isinstance(all_data, tuple):
         raise ValueError(
             "Expected construct_graph to return a tuple when should_split is True."
         )
-    base_data, fold_data, test_data = all_graph_data
+    base_data, fold_data, test_data, calib_data = all_data
 
     # diagnose_data_leakage(fold_data, test_data)
     # check_isolated_components(fold_data, test_data)
@@ -136,6 +136,7 @@ def combine_split_data(
     export_all_graphs_to_html(
         fold_data,
         test_data,
+        calib_data,
         coordinates,
         base_params["k_nearest"],
         base_params["connection_radius"],
@@ -157,6 +158,7 @@ def combine_split_data(
         torch.save(base_data, os.path.join(output_path, "base_data.pt"))
         torch.save(fold_data, os.path.join(output_path, "fold_data.pt"))
         torch.save(test_data, os.path.join(output_path, "test_data.pt"))
+        torch.save(calib_data, os.path.join(output_path, "calib_data.pt"))
         print(f"✓ All files successfully saved to {paths['output']}.")
     except Exception as e:
         print(f"Error saving files: {e}")

src/stages/train.py
@@ -4,7 +4,12 @@ import os
 import torch
 
 from src.models import get_model
-from src.utilities.general_utils import LogTime, ensure_directory_exists, load_params
+from src.utilities.general_utils import (
+    LogTime,
+    ensure_directory_exists,
+    load_data,
+    load_params,
+)
 from src.utilities.train_utils import train
 
 
@@ -43,10 +48,7 @@ def run_training(paths, params, model_name, cycle_num):
     output_path = ensure_directory_exists(paths["output"])
 
     # Load training data from previous cycle
-    fold_data_path = os.path.join(paths["base_data"], "fold_data.pt")
-    if not os.path.exists(fold_data_path):
-        raise FileNotFoundError(f"Training data not found at {fold_data_path}")
-    fold_data = torch.load(fold_data_path, weights_only=False)
+    fold_data = load_data(os.path.join(paths["base_data"], "fold_data.pt"))
 
     # Initialize model
     model = get_model(model_name, model_params)
@@ -57,11 +59,11 @@ def run_training(paths, params, model_name, cycle_num):
     # Training loop
     fold_results = []
 
-    for graph_idx, graph in enumerate(fold_data, 1):
-        print(f"\nTraining model on fold {graph.fold + 1}...")
+    for fold_idx, fold in enumerate(fold_data, 1):
+        print(f"\nTraining model on fold {fold_idx}...")
 
         trained_model, best_loss_val = train(
-            graph.to(device),
+            fold,
             model.to(device),
             n_classes=n_classes,
             n_epochs=train_params["n_epochs"],
@@ -77,7 +79,7 @@ def run_training(paths, params, model_name, cycle_num):
             patience_early_stopping=train_params["patience_early_stopping"],
             min_delta_early_stopping=train_params["min_delta_early_stopping"],
             save_path=output_path,
-            dataset_idx=graph_idx,
+            dataset_idx=fold_idx,
         )
 
         fold_results.append((trained_model, best_loss_val))

src/utilities/eval_utils.py
@@ -37,7 +37,8 @@ from src.utilities.calibration_utils import (
 
 
 def evaluate_with_calibration(
-    data,
+    test_data,
+    calibration_data,
     model,
     calibration_method: Literal[
         "temperature", "isotonic", "platt", "beta", "dirichlet"
@@ -55,7 +56,6 @@ def evaluate_with_calibration(
     patience_learning_rate_scheduler: int,
     patience_early_stopping: int,
     min_delta_early_stopping: float,
-    device: torch.device = torch.device("cpu"),
     verbose: bool = False,
     seed: int = 42,
     save_path: str | None = None,
@@ -112,8 +112,9 @@ def evaluate_with_calibration(
         - Saves calibrator model to '{save_path}/calibrator.pkl'
 
     """
-    y_true_test = data.y[data.test_mask]
-    y_tru_calib = data.y[data.calib_mask]
+    device = next(model.parameters()).device
+    y_true_test = test_data.y
+    y_tru_calib = calibration_data.y
     sample_weights_test = compute_sample_weight("balanced", y_true_test.cpu())
     sample_weights_test = torch.tensor(sample_weights_test, dtype=torch.float32).to(
         device
@@ -124,38 +125,37 @@ def evaluate_with_calibration(
     )
     # Get base model predictions
     model.eval()
-    logits = model(data)
-    base_probs = F.softmax(logits, dim=1)
+    test_logits = model(test_data)
+    calib_logits = model(calibration_data)
     # Calculate metrics before calibration
-    uncal_probs = base_probs[data.test_mask]
-    uncal_pred = uncal_probs.argmax(dim=1)
+    uncalibrated_probs = F.softmax(test_logits, dim=1)
+    uncalibrated_pred = uncalibrated_probs.argmax(dim=1)
 
     cal_metrics = CalibrationMetrics(n_bins=n_bins)
     cal_metrics_uncalibrated = cal_metrics.calculate_metrics(
-        uncal_probs, y_true_test, sample_weights_test
+        uncalibrated_probs, y_true_test, sample_weights_test
     )
     uncal_metrics = {
         "acc": accuracy_score(
             y_true_test.cpu().numpy(),
-            uncal_pred.cpu().numpy(),
+            uncalibrated_pred.cpu().numpy(),
             sample_weight=sample_weights_test.cpu().numpy(),
         ),
         "f1": fbeta_score(
             y_true_test.cpu().numpy(),
-            uncal_pred.cpu().numpy(),
+            uncalibrated_pred.cpu().numpy(),
             sample_weight=sample_weights_test.cpu().numpy(),
             beta=0.5,
             average="macro",
         ),
         "mcc": matthews_corrcoef(
             y_true_test.cpu().numpy(),
-            uncal_pred.cpu().numpy(),
+            uncalibrated_pred.cpu().numpy(),
             sample_weight=sample_weights_test.cpu().numpy(),
         ),
         "ece": cal_metrics_uncalibrated.ece,
         "mce": cal_metrics_uncalibrated.mce,
     }
-    calib_logits = logits[data.calib_mask]
     if save_path is None:
         save_path = "results/evaluation"
     os.makedirs(save_path, exist_ok=True)
@@ -182,25 +182,25 @@ def evaluate_with_calibration(
         seed=seed,
         save_path=save_path,
     ).save(filepath=save_path)
-    cal_probs = pipeline.predict_from_logits(logits[data.test_mask])
-    cal_pred = cal_probs.argmax(dim=1)
+    calibrated_probs = pipeline.predict_from_logits(test_logits)
+    calibrated_pred = calibrated_probs.argmax(dim=1)
     cal_metrics_calibrated = cal_metrics.calculate_metrics(
-        cal_probs, y_true_test, sample_weights_test, verbose=False
+        calibrated_probs, y_true_test, sample_weights_test, verbose=False
     )
     cal_mcc = matthews_corrcoef(
         y_true_test.cpu().numpy(),
-        cal_pred.cpu().numpy(),
+        calibrated_pred.cpu().numpy(),
         sample_weight=sample_weights_test.cpu().numpy(),
     )
     cal_metrics = {
         "acc": accuracy_score(
             y_true_test.cpu().numpy(),
-            cal_pred.cpu().numpy(),
+            calibrated_pred.cpu().numpy(),
             sample_weight=sample_weights_test.cpu().numpy(),
         ),
         "f1": fbeta_score(
             y_true_test.cpu().numpy(),
-            cal_pred.cpu().numpy(),
+            calibrated_pred.cpu().numpy(),
             sample_weight=sample_weights_test.cpu().numpy(),
             beta=0.5,
             average="macro",
@@ -224,7 +224,7 @@ def evaluate_with_calibration(
     print(f"Saving confusion matrix to {confusion_path} ...")
     plot_confusion_matrix(
         y_true_test.cpu().numpy(),
-        cal_pred.cpu().numpy(),
+        calibrated_pred.cpu().numpy(),
         class_names,
         title=f"Matthews correlation coefficient {cal_mcc:.3f}",
         save_path=confusion_path,

src/utilities/pred_utils.py
@@ -23,8 +23,8 @@ def prediction(
     calibrator_path: str,
     class_names: list,
     save_path: str | None = None,
-    device: torch.device = torch.device("cpu"),
 ):
+    device = next(model.parameters()).device
     # hide the labels from model
     hidden_labels = pred_data.y
     pred_data.y = None

src/utilities/train_utils.py
@@ -72,8 +72,13 @@ def train(
         - Comprehensive training visualization plots
 
     """
-    y_true_train = data.y[data.train_mask]
-    device = y_true_train.device
+    device = next(model.parameters()).device
+
+    train_data, val_data = data
+    train_data = train_data.to(device)
+    val_data = val_data.to(device)
+    y_true_train = train_data.y
+    y_true_val = val_data.y
 
     class_weights_train = compute_class_weight(
         class_weight="balanced",
@@ -83,7 +88,7 @@ def train(
     criterion_train = nn.CrossEntropyLoss(
         weight=torch.tensor(class_weights_train, dtype=torch.float32).to(device)
     )
-    y_true_val = data.y[data.val_mask]
+
     class_weights_val = compute_class_weight(
         class_weight="balanced",
         classes=np.arange(n_classes),
@@ -125,13 +130,11 @@ def train(
     early_stopping = EarlyStopping(
         patience=patience_early_stopping, min_delta=min_delta_early_stopping
     )
-
+    # Training phase
+    model.train()
     for epoch in range(n_epochs):
-        # Training phase
-        model.train()
         optimizer.zero_grad()
-        logits = model(data)
-        logits_train = logits[data.train_mask]
+        logits_train = model(train_data)
         loss = criterion_train(logits_train, y_true_train)
         loss.backward()
 
@@ -155,8 +158,7 @@ def train(
         # Evaluation phase
         model.eval()
         with torch.no_grad():
-            logits = model(data)
-            logits_val = logits[data.val_mask]
+            logits_val = model(val_data)
             y_prob_val = torch.softmax(logits_val, dim=1)
             loss_val = criterion_val(logits_val, y_true_val)
 